6424fa400f6f657b878ecc30390e20873045f6fd
jrletner <jrletner@gmail.com>
2024-06-12 16:15:23 -0500

    refactor: Initialize course goals as an empty array in App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import GoalList from './components/GoalList/GoalList';
 
 export default function App() {
   // define a state variable to store the course goals
-  const [courseGoals, setCourseGoals] = useState([
-    { id: 'cg1', text: 'Get started writing code in React', dueDate: '2021-12-31' },
-    { id: 'cg2', text: 'Learn everything you can learn about React', dueDate: '2022-12-31' },
-    { id: 'cg3', text: 'Practice, practice, practice React', dueDate: '2023-12-31' },
-  ]);
+  const [courseGoals, setCourseGoals] = useState([]);
 
   // this is a callback function that will be passed to NewGoal component in order to handle the new goal once it is submitted
   function addNewGoalHandler(newGoal) {
